927dcb4848fb05e0210987af18fb63489317bca4
Mark Strancheuski <mariknowhere@gmail.com>
2022-08-01 22:05:04 +0300

    C-2: Bug fixed!

--- src/client/hooks/useEmailValidate.ts
+++ src/client/hooks/useEmailValidate.ts
@@ -1,13 +1,21 @@
-import {KeyboardEvent, useState} from "react";
+import {KeyboardEvent, useEffect, useState} from "react";
 import {EMAIL_REGEXP} from "../constants/form";
 
 export const useEmailValidate = () => {
   const [emailState, setEmailState] = useState<string>('');
-  const [isEmailCorrect, setIsEmailCorrect] = useState<boolean>(true);
+  const [isEmailCorrect, setEmailCorrect] = useState<boolean>(true);
+
+  useEffect(() => {
+    const splitEmail = emailState.split(' ').length === 2;
+
+    if (splitEmail) {
+      setEmailCorrect(false)
+    }
+  }, [emailState]);
 
   const onEmailChangeHandler = ((event: KeyboardEvent<HTMLInputElement>) => {
     setEmailState(event.target.value);
-    setIsEmailCorrect(EMAIL_REGEXP.test(event.target.value));
+    setEmailCorrect(EMAIL_REGEXP.test(event.target.value));
   })
 
   return {  emailState, setEmailState, onEmailChangeHandler, isEmailCorrect }
